9d548171989d9084756dac490540082c16f06f29
Michee Luzayisu <michemakaya11@gmail.com>
2025-07-02 02:38:00 +0200

    Create the contact model

--- env/backend/core/models.py
+++ env/backend/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=155)
+    email = models.EmailField(max_length=255)
+    message = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
