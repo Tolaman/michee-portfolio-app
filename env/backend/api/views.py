bd7277d4ba79718e068a9cee829eef0a7c2a39f6
Michee Luzayisu <michemakaya11@gmail.com>
2025-07-02 03:18:19 +0200

    create the action for returning the data

--- env/backend/api/views.py
+++ env/backend/api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Project, Service, Experience
+from .serializer import ProjectSerializer, ServiceSerializer, ExperienceSerializer
 
 # Create your views here.
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing project details.
+    """
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+class ServiceViewSet(viewsets.ModelViewSet):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
+
+class ExperienceViewSet(viewsets.ModelViewSet):
+    queryset = Experience.objects.all()
+    serializer_class = ExperienceSerializer
--- env/backend/core/views.py
+++ env/backend/core/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import viewsets # APIView 
+#from rest_framework.response import Response
+from .models import Contact
+from .serializer import ContactSerializer
 
 # Create your views here.
+class ContactViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing contact messages.
+    """
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
+
+    # Uncomment if you want to customize the response
+    # def list(self, request):
+    #     queryset = self.get_queryset()
+    #     serializer = self.get_serializer(queryset, many=True)
+    #     return Response(serializer.data)
